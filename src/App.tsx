a30add7904e1e5774ea143ff277cf689f2b5f590
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-06-02 11:05:18 +0300

    Add project UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,52 @@
+import "./global.scss";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <main id="main">
+      <section className="card">
+        <DateInputs />
+        <div className="breakline">
+          <img src="images/icon-arrow.svg" alt="icon-arrow" />
+        </div>
+        <Output />
+      </section>
+    </main>
+  );
+}
+
+function DateInputs() {
+  return (
+    <div className="date-inputs">
+      <div>
+        <label>DAY</label>
+        <input type="text" placeholder="DD" />
+      </div>
+      <div>
+        <label>MONTH</label>
+        <input type="text" placeholder="MM" />
+      </div>
+      <div>
+        <label>YEAR</label>
+        <input type="text" placeholder="YYYY" />
+      </div>
+    </div>
+  );
+}
+
+function Output() {
+  return (
+    <div className="output">
+      <p>
+        <span>- - </span> years
+      </p>
+      <p>
+        <span>- - </span> months
+      </p>
+      <p>
+        <span>- - </span> days
+      </p>
+    </div>
+  );
 }
 
 export default App;
