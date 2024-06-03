6b6755bdc5ba62863740673e5affc877ef8c5625
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-06-03 11:25:04 +0300

    Add project logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,100 @@
+import { useState } from "react";
 import "./global.scss";
 
+interface DateInputProps {
+  birthYear: number | string;
+  setBirthYear: (val: number) => void;
+  birthMonth: number | string;
+  setBirthMonth: (val: number) => void;
+  birthDay: number | string;
+  setBirthDay: (val: number) => void;
+}
+
+interface OutputProps {
+  birthYear: number;
+  birthMonth: number;
+  birthDay: number;
+}
+
 function App() {
+  const [birthYear, setBirthYear] = useState<string | number>("");
+  const [birthMonth, setBirthMonth] = useState<string | number>("");
+  const [birthDay, setBirthDay] = useState<string | number>("");
   return (
     <main id="main">
       <section className="card">
-        <DateInputs />
+        <DateInputs
+          birthYear={birthYear}
+          setBirthYear={setBirthYear}
+          birthMonth={birthMonth}
+          setBirthMonth={setBirthMonth}
+          birthDay={birthDay}
+          setBirthDay={setBirthDay}
+        />
         <div className="breakline">
           <img src="images/icon-arrow.svg" alt="icon-arrow" />
         </div>
-        <Output />
+        <Output birthYear={+birthYear} birthMonth={+birthMonth} birthDay={+birthDay} />
       </section>
     </main>
   );
 }
 
-function DateInputs() {
+function DateInputs(props: DateInputProps) {
   return (
     <div className="date-inputs">
       <div>
         <label>DAY</label>
-        <input type="text" placeholder="DD" />
+        <input
+          type="text"
+          placeholder="DD"
+          value={props.birthDay}
+          onChange={(e) => props.setBirthDay(+e.target.value)}
+        />
       </div>
       <div>
         <label>MONTH</label>
-        <input type="text" placeholder="MM" />
+        <input
+          type="text"
+          placeholder="MM"
+          value={props.birthMonth}
+          onChange={(e) => props.setBirthMonth(+e.target.value)}
+        />
       </div>
       <div>
         <label>YEAR</label>
-        <input type="text" placeholder="YYYY" />
+        <input
+          type="text"
+          placeholder="YYYY"
+          value={props.birthYear}
+          onChange={(e) => props.setBirthYear(+e.target.value)}
+        />
       </div>
     </div>
   );
 }
 
-function Output() {
+function Output({ birthYear, birthMonth, birthDay }: OutputProps) {
+  const today = new Date();
+  const year = today.getFullYear() - birthYear;
+  let month = today.getMonth() + 1 - birthMonth;
+  const day = Math.abs(today.getDate());
+  console.log(day);
+
+  if (month < 0) {
+    month += 12;
+  }
+
   return (
     <div className="output">
       <p>
-        <span>- - </span> years
+        <span>{birthYear > 1920 && birthYear < new Date().getFullYear() ? year : "- -"} </span> years
       </p>
       <p>
-        <span>- - </span> months
+        <span>{birthMonth > 0 && birthMonth < 13 ? month : "- -"} </span> months
       </p>
       <p>
-        <span>- - </span> days
+        <span>{birthDay > 0 && birthDay < 31 ? day : "- - "} </span> days
       </p>
     </div>
   );
